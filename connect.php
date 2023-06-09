18d470d418943e1346abf91bd21a78d8f6021eb4
9573030999 <127820366+9573030999@users.noreply.github.com>
2023-06-09 15:31:01 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$hostname  = 'localhost';
-$username = 'root';
-$password='';
-$dbname = 'train';
+$hostname  = 'database-1.c20hgj6ncxej.us-east-1.rds.amazonaws.com';
+$username = 'mticket';
+$password='mticket90';
+$dbname = 'mticket';
 $connect =  mysqli_connect($hostname , $username , $password ,$dbname) or die("Error Connecting");
 ?>
